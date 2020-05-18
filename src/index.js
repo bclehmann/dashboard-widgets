18e3410cc756182838deb6ed7fbfb0f9663f00cb
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-05-18 16:06:48 -0600

    Got MVP working, babel compiling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-exports.version = () => process.env.npm_package_version
+import React from 'react'
+
+export default "The default export of dashboard-widgets does nothing. Import the module you want explicitly, or use import * as dw"
+
+export class Test extends React.Component{
+	render(){
+		return <h1>dashboard-widgets says Hello World</h1>
+	}
+}
